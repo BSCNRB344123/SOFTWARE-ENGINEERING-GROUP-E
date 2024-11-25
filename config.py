dca949f71bd9756a17833365e3cb41c51a15a0c6
gloriaabineza <abinezagloria@gmail.com>
2024-11-25 12:29:40 +0300

    deleted files with no comments

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///health_center.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
